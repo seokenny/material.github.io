16cc94aeb1b0560a7b03e4b21963eec9ecdf73e8
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2020-05-01 18:19:14 -0400

    finished base

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".nav__burger").addEventListener("click",function(){
+    document.querySelector(".nav__burger").classList.toggle("nav-active");
+    document.querySelector(".nav__burger-plus").classList.add("plus-active");
+});
