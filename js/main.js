9f44ff60dece8832dc48b3d34f24be1a30e51329
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2020-05-02 12:48:51 -0400

    lax and circles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+
 document.querySelector(".nav__burger").addEventListener("click",function(){
     document.querySelector(".nav__burger").classList.toggle("nav-active");
     document.querySelector(".nav__burger-plus").classList.add("plus-active");
 });
+
+window.onload = function() {
+	lax.setup() // init
+
+	const updateLax = () => {
+		lax.update(window.scrollY)
+		window.requestAnimationFrame(updateLax)
+	}
+
+	window.requestAnimationFrame(updateLax)
+}
