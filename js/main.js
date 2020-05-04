1c7df3f584f738fcb93a96edeb14c53f8edcfd26
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2020-05-03 22:09:36 -0400

    desktop nav scrolls

--- js/main.js
+++ js/main.js
@@ -18,3 +18,42 @@ window.onload = function() {
 
 	window.requestAnimationFrame(updateLax)
 }
+
+
+function scrollAbout() {
+	console.log(event);
+    var goTo = $(".aboutme");
+        var top = goTo.offset().top;
+        top-=75;
+        $('html,body').animate({scrollTop: top}, 'slow');
+        return false;
+}
+
+var navArray = ["nav__portfolio", "nav__about", "nav__contact"];
+
+var sectionArray = [".portfolio", ".aboutme", "footer"];
+
+function scrollTo() {
+	var clicked;
+
+	var goTo = "";
+	clicked = event.target.classList;
+	for(var i = 0; i < navArray.length; i++){
+		if(clicked[0] == navArray[i]){
+			goTo = $(sectionArray[i]);
+			console.log(goTo);
+		}
+	}
+
+	
+		var top = goTo.offset().top;
+		top-=70;
+        $('html,body').animate({scrollTop: top}, 'slow');
+        return false;
+}
+
+
+
+document.querySelector(".nav__list").addEventListener("click", function(){
+	scrollTo();
+});
